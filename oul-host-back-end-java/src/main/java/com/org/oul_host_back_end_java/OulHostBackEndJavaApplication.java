dec3efc6790b9601bba739d7fdff6ae9e33c8124
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-12-16 16:29:35 -0300

    Initial commit

---
+++ oul-host-back-end-java/src/main/java/com/org/oul_host_back_end_java/OulHostBackEndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.org.oul_host_back_end_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OulHostBackEndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OulHostBackEndJavaApplication.class, args);
+	}
+
+}
