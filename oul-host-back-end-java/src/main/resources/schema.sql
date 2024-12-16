749e5f507bdf9616408dce6d3e33502813f9b223
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-12-16 17:52:46 -0300

    add swagger, hibernate and validation

---
+++ oul-host-back-end-java/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS players (
+id VARCHAR NOT NULL,
+name VARCHAR(50) NOT NULL,
+email VARCHAR(30) NOT NULL,
+telephone VARCHAR(20) NULL,
+player_type INT NOT NULL,
+CONSTRAINT player_primary_key PRIMARY KEY (id)
+);
