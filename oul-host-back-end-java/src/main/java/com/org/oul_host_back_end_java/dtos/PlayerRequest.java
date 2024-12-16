ded834d80467ef7612d66f656c95c33ec4b842dc
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-12-16 17:57:56 -0300

    commited by Abner

--- oul-host-back-end-java/src/main/java/com/org/oul_host_back_end_java/dtos/PlayerRequest.java
+++ oul-host-back-end-java/src/main/java/com/org/oul_host_back_end_java/dtos/PlayerRequest.java
@@ -2,6 +2,10 @@ package com.org.oul_host_back_end_java.dtos;
 
 import com.org.oul_host_back_end_java.enums.PlayerType;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +14,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PlayerRequest {
+	@NotEmpty
+	@NotNull
 	private String name;
+	@NotEmpty
+	@NotNull
 	private String email;
+	@NotEmpty
 	private String telephone;
+	@Enumerated(EnumType.STRING)
 	private PlayerType playerType;
 }
