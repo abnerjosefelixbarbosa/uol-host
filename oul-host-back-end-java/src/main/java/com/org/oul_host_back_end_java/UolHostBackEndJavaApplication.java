79b73bc1dacff75fc7d9801ba5662b165be741c2
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-12-22 12:19:48 -0300

    commited by Abner

--- oul-host-back-end-java/src/main/java/com/org/oul_host_back_end_java/UolHostBackEndJavaApplication.java
+++ oul-host-back-end-java/src/main/java/com/org/oul_host_back_end_java/UolHostBackEndJavaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OulHostBackEndJavaApplication {
+public class UolHostBackEndJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OulHostBackEndJavaApplication.class, args);
+		SpringApplication.run(UolHostBackEndJavaApplication.class, args);
 	}
 
 }
