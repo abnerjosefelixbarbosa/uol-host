8a47c7fd96330f973095348bb08295f9fb8cc8f3
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-12-28 15:14:29 -0300

    commited by Abner

--- oul-host-back-end-spring-boot-java/src/main/java/com/org/uol_host_back_end_spring_boot_java/config/CorsConfig.java
+++ oul-host-back-end-spring-boot-java/src/main/java/com/org/uol_host_back_end_spring_boot_java/config/CorsConfig.java
@@ -1,9 +1,13 @@
 package com.org.uol_host_back_end_spring_boot_java.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class CorsConfig {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
