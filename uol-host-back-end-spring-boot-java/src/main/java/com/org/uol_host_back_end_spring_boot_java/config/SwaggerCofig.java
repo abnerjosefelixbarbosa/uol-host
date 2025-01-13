6f4c03ef2186af98c7e045ed3858d300c8fddb17
ABNER JOSÉ FELIX BARBOSA <abnerbarbosa95@gmail.com>
2025-01-13 16:23:06 -0300

    update SwaggerCofig.java

--- uol-host-back-end-spring-boot-java/src/main/java/com/org/uol_host_back_end_spring_boot_java/config/SwaggerCofig.java
+++ uol-host-back-end-spring-boot-java/src/main/java/com/org/uol_host_back_end_spring_boot_java/config/SwaggerCofig.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
-public class SwaggerCofig {
+public class SwaggerConfig {
 	@Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
